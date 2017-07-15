f06427f1a6ab5dfb2d684a75a671b959d4db0fa5
Luiz Maffeis <luiz.maffeis@gmail.com>
2017-07-14 22:53:27 -0300

    user list and detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { UserdetailComponent } from './userdetail/userdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlistComponent,
+    UserdetailComponent
   ],
   imports: [
     BrowserModule
